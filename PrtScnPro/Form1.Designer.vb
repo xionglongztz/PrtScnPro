2f9b048a5eb5b8c19dcf33f39e1bc30a4933d623
reece00 <37351410+reece00@users.noreply.github.com>
2023-03-27 20:11:14 +0800

    添加固定路径

--- PrtScnPro/Form1.Designer.vb
+++ PrtScnPro/Form1.Designer.vb
@@ -30,13 +30,16 @@ Partial Class Form1
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(11, 42)
+        Me.Button1.Location = New System.Drawing.Point(8, 34)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(181, 96)
+        Me.Button1.Size = New System.Drawing.Size(136, 77)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "复制到剪贴板(&C)"
         Me.Button1.UseVisualStyleBackColor = True
@@ -44,17 +47,19 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(8, 14)
+        Me.Label1.Location = New System.Drawing.Point(6, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(129, 15)
+        Me.Label1.Size = New System.Drawing.Size(131, 12)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "按住Ctrl选择区域"
+        Me.Label1.Text = "在此窗口按住S选择区域"
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(198, 42)
+        Me.Button2.Location = New System.Drawing.Point(148, 34)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(180, 96)
+        Me.Button2.Size = New System.Drawing.Size(135, 77)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "保存为文件(&F)"
         Me.Button2.UseVisualStyleBackColor = True
@@ -69,24 +74,48 @@ Partial Class Form1
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(11, 144)
+        Me.CheckBox1.Location = New System.Drawing.Point(8, 115)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(89, 19)
+        Me.CheckBox1.Size = New System.Drawing.Size(72, 16)
         Me.CheckBox1.TabIndex = 3
         Me.CheckBox1.Text = "窗口置顶"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(84, 115)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(96, 16)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "使用固定路径"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoEllipsis = True
+        Me.Label2.Location = New System.Drawing.Point(11, 147)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(272, 80)
+        Me.Label2.TabIndex = 6
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(388, 170)
+        Me.ClientSize = New System.Drawing.Size(292, 236)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBox2)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "截屏工具"
@@ -101,4 +130,6 @@ Partial Class Form1
     Friend WithEvents Timer1 As Timer
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label2 As Label
 End Class
--- PrtScnPro/Form2.vb
+++ PrtScnPro/Form2.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form2
     Private Sub Form2_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
-        If e.KeyCode = Keys.ControlKey Then '如果释放Ctrl
+        If e.KeyCode = Keys.S Then '如果释放Ctrl
             Form1.Timer1.Enabled = False '关闭计时器
             With Form1
                 .Label1.Text = "区域已选择，你可以：" '显示提示
@@ -12,6 +12,6 @@
     End Sub
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TopMost = True '置顶防止忽略
-        Form1.Label1.Text = "正在绘制区域，松开Ctrl可完成选择" '显示提示
+        Form1.Label1.Text = "正在绘制区域，松开S可完成选择" '显示提示
     End Sub
 End Class
